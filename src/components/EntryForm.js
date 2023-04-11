a89cc9d3560f00b39ef47d855361f2b4a0e41f26
Henrike Stahlhut <henrike.st@icloud.com>
2023-04-11 12:53:03 +0200

    add styling fprm

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -4,9 +4,9 @@ export default function Form (){
     return (
         <form className='form'>
             <label htmlFor="motto" className='form__label'>Motto</label>
-            <input type="text" id="motto"></input>
+            <input type="text" id="motto" className='form__field'></input>
             <label htmlFor="textarea" className='form__label'>Notes</label>
-            <textarea cols="30" rows="5" id="textarea"></textarea>
+            <textarea cols="30" rows="5" id="textarea" className='form__field'></textarea>
             <button type="submit" className='form__button'>Create</button>
         </form>
     )
